02773c6bc5cc21e502181d5bed1e3788fa27e1ef
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-03-22 23:04:57 +0100

    Added method

--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 class Movie {
   public $title;
   public $genre;
-  public $lang;
+  public $score;
 
-  function __construct($_title) {
+  function __construct($_title, $_genre, $_score = 0) {
     $this->title = $_title;
+    $this->genre = $_genre;
+    $this->score = $_score;
+  }
+
+  function getScore() {
+    if ($this->score > 50) return 'La serie ha un voto sopra la media';
+    if ($this->score <= 50) return 'La serie ha un voto sotto la media';
   }
 }
 
-$movie1 = new Movie('Ritorno al futuro');
-echo $movie1->title;
+$movie1 = new Movie('Breaking Bad', 'Dramma', 100);
+$movie2 = new Movie('Barbie', 'Commedia', 0); // Nulla contro Barbie :(
+$get_valutation1 = $movie1->getScore();
+$get_valutation2 = $movie2->getScore();
+echo $movie1->title . ' Valutazione: ' . $get_valutation1 . '<br>';
+echo $movie2->title . ' Valutazione: ' . $get_valutation2;
 ?>
 
 <!DOCTYPE html>
